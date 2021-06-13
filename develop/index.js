309ce4cd706703e2f902412114672770b0859433
Mark S <marksherm04@gmail.com>
2021-06-13 15:47:41 -0400

    added first few lines of code in index.js, testing arrays

--- develop/index.js
+++ develop/index.js
@@ -1,13 +1,21 @@
 // TODO: Include packages needed for this application
+const userProfile = process.argv.slice(2, process.argv.length);
+console.log(userProfile)
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// // TODO: Create an array of questions for user input
+// const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
+
+const printProfileData = (profileDataArr) => {
+	console.log(profileDataArr);
+};
+
+printProfileData(userProfile);
